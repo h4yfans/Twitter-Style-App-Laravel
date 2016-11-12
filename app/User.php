176604594e0aa71552ac084079edd355651367c6
Kaan Karaca <kaan94karaca@gmail.com>
2016-11-12 18:30:57 +0300

    setting up follow functionality

--- app/User.php
+++ app/User.php
@@ -27,6 +27,11 @@ class User extends Authenticatable
         return $this->hasMany('App\Post');
     }
 
+    public function following()
+    {
+        return $this->belongsToMany('App\User', 'follows', 'user_id', 'follower_id');
+    }
+
     public function getAvatar()
     {
         return 'https://www.gravatar.com/avatar/' . md5($this->email) . 'x?s=45&d=mm';
@@ -45,5 +50,24 @@ class User extends Authenticatable
     public function getRouteKeyName()
     {
         return 'username';
+    }
+
+    public function isNot(User $user)
+    {
+        return $this->id !== $user->id;
+    }
+
+    public function isFollowing(User $user)
+    {
+        return (bool)$this->following->where('id', $user->id)->count();
+    }
+
+    public function canFollow(User $user)
+    {
+        if ($this->isNot($user)) {
+            return false;
+        }
+
+        return !$this->isFollowing($user);
     }
 }
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,11 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <h3>{{$user->username}}</h3>
-                </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h3>{{$user->username}}</h3>
+
+                @if(Auth::user()->isNot($user))
+                    @if(Auth::user()->isFollowing($user))
+
+                    @else
+
+                    @endif
+                @endif
             </div>
         </div>
+    </div>
 @endsection
