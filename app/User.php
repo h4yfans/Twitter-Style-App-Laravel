5070a35bb5942fb8f812b4520a4db104ad28c288
Kaan Karaca <kaan94karaca@gmail.com>
2016-11-12 17:12:06 +0300

    creating the user profile

--- app/User.php
+++ app/User.php
@@ -32,4 +32,8 @@ class User extends Authenticatable
     public function getAvatarAttribute(){
         return $this->getAvatar();
     }
+    
+    public function getRouteKeyName(){
+        return 'username';
+    }
 }
